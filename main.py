758a850c5de6382edfdcffe578ebdd298da646ac
JFGrobusch <61617930+JFGrobusch@users.noreply.github.com>
2020-12-14 11:33:08 +0100

    work save

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ modification pending
 
 from tools import getarray, savearray
 import numpy as np
+import sys
 
 class datapoint():
     def __init__(self, angle, dim):
@@ -26,6 +27,7 @@ class datapoint():
 
 def getprecession(datapoint):
     def avgdiff(row, reach): #considers [reach] entries preceding and following points in the row to find average of entries
+#diffrow array has max value 255 min value 0 and all int; low level of fineness recorded. consider converting array to floats, this will slow down calcs
         index = 0
         diffrow = np.zeros_like(row) #gen empty array to write to
         for entry in row:
@@ -36,19 +38,29 @@ def getprecession(datapoint):
             following_points = row[index+1:high] #get points before and after point
             avg_pre = np.mean(preceding_points)
             avg_follow = np.mean(following_points) #get averages
-            if avg_pre.size < 10 or avg_follow.size < 10: np.put(diffrow, index, 0) #check that array is suitably large (avoid noise)
-            else: np.put(diffrow, index, abs(avg_pre-avg_follow))    #returns diffrow              
-            index += 1       
+            if preceding_points.size < 10 or following_points.size < 10: np.put(diffrow, index, 0) #check that array is suitably large (avoid noise)
+            else: np.put(diffrow, index, abs(avg_pre-avg_follow))    #returns diffrow; only int are recorded, floats are unnecessarily slow           
+            index += 1
         return(diffrow)
+    print("Generating difference array for", datapoint.name, "deg AoA", datapoint.dim, "case")
     array = getarray(datapoint.name, datapoint.dim) #initialise array
     crop = 80 #number of pixels removed from top and bottom of image (crop). number hard coded
     array = array[crop:array.shape[0]-crop] #crops array to desired dimension
-    diffarray = array #copies initial array to new array for modification
+    
+    reach = 30 #reach concerns the number of pixels considered in positive and negative direction
+    diffarray = np.zeros_like(array) #copies initial array to new array for modification
+    
+    #generates new array composed of the difference between the average of 30 preceding and following entries
+    index = 0
+    for row in array:
+        diffarray[index] = avgdiff(row, reach)#gets diffrow, writes to np row
+        index += 1 
+            
+    #refine edges; narrow down to one leading edge, one trailing edge, and transition.
+    def refineedge(row, reach)
     index = 0
     for row in diffarray:
-        np.put(diffarray, index, avgdiff(row, 30)) #gets diffrow, writes to np row. using reach (0.5range) of 30 px
-        index += 1
-    #TROUBLESHOOT; DATA IS NOT CHANGING
+        diffarray[index] = 1
     savearray(diffarray, 'test', datapoint.name, datapoint.dim)    
             
     return()
