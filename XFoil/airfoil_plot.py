035f2af3215fd63cbb73704847c436d43c034013
George Kovar <kovargeorge@gmail.com>
2021-01-05 11:52:06 +0100

    Subplots implemented

--- XFoil/airfoil_plot.py
+++ XFoil/airfoil_plot.py
@@ -15,7 +15,6 @@ df = pd.read_csv("sim.csv", skiprows=10, sep="  ", engine="python")
 # skips the data info rows and orders data into columns creating a dataframe df
 
 df = df.drop(0)  # drops the row with dashed lines from XFoil
-# df.to_csv("sim2.csv", sep=" ", columns=["alpha"])
 # now the dataframe has a first row designated with 1 instead of 0
 
 alpha = df.alpha.to_numpy()  # the data is in strings by default
@@ -37,20 +36,23 @@ CM = np.array([float(i) for i in CM])  # convert
 # plt.ylabel("Coefficient of lift [-]")
 # plt.show()
 
-fig, axs = plt.subplots(2, 2)
+plots_x = 2  # plots in the x direction
+plots_y = 2  # plots in the y direction
+fig, axs = plt.subplots(plots_x, plots_y)
 fig.suptitle('XFoil Simulation Results')
+fig.legend("XFoil")
 fig.tight_layout(pad=3)
 axs[0, 0].plot(alpha, CL)
 axs[0, 0].set_title('CL vs. alpha')
-axs[0, 1].plot(CL, CD, 'tab:orange')
-axs[0, 1].set_title('CD vs. CL')
+axs[0, 1].plot(CD, CL, 'tab:orange')
+axs[0, 1].set_title('CL vs. CD')
 axs[1, 0].plot(alpha, CM, 'tab:green')
 axs[1, 0].set_title('CM vs. alpha')
 axs[1, 1].plot(alpha, CD, 'tab:red')
-axs[1, 1].set_title('CD vs. alpha cause why not')
+axs[1, 1].set_title('CD vs. alpha')
 
 axs[0, 0].set(xlabel="Angle of attack [deg]", ylabel="Coefficient of lift [-]")
-axs[0, 1].set(xlabel="Coefficient of lift [-]", ylabel="Coefficient of drag [-]")
+axs[0, 1].set(xlabel="Coefficient of drag [-]", ylabel="Coefficient of lift [-]")
 axs[1, 0].set(xlabel="Angle of attack [deg]", ylabel="Coefficient of moment [-]")
 axs[1, 1].set(xlabel="Angle of attack [deg]", ylabel="Coefficient of drag [-]")
 
