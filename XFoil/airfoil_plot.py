c5b86626bac6853fcafc61f0b94b3d3289543355
George Kovar <kovargeorge@gmail.com>
2021-01-06 06:49:38 +0100

    Subplots implemented

--- XFoil/airfoil_plot.py
+++ XFoil/airfoil_plot.py
@@ -2,13 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
-
-"""
-todo implement turbulence
-todo implement viscosity
-    is part of xfoil, use cmd v 
-"""
-
+# --- Get XFoil data --- #
 pd.set_option('display.expand_frame_repr', False)
 df_raw = pd.read_csv("sim.csv")  # raw output of XFoil from the polar mode
 
@@ -20,10 +14,7 @@ print(df)
 # df.to_csv("sim2.csv", sep=" ", columns=["alpha"])
 # now the dataframe has a first row designated with 1 instead of 0
 
-
-"""
-Get xfoil data
-"""
+# --- Process XFoil data --- #
 alpha = df.alpha.to_numpy()  # the data is in strings by default
 alpha = np.array([float(i) for i in alpha])  # convert
 
@@ -36,12 +27,7 @@ CD = np.array([float(i) for i in CD])  # convert
 CM = df.CM.to_numpy()  # the data is in strings by default
 CM = np.array([float(i) for i in CM])  # convert
 
-"""
-Get test data
-"""
-import data_analysis_2D as td
-
-#plot
+# --- Plot --- #
 plots_x = 2
 plots_y = 2
 fig, axs = plt.subplots(plots_x, plots_y, figsize=(8, 8))
@@ -69,4 +55,3 @@ axs[1, 1].set(xlabel="Angle of attack [deg]", ylabel="Coefficient of drag [-]")
 
 fig.show()
 fig.savefig("test")
-
